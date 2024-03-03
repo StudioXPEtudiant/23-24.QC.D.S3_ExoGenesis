132c47fe324ba17425287b1b3a7d46348f3f6027
JDGSP <121699465+JDGSP@users.noreply.github.com>
2024-03-03 15:28:49 -0500

    PlayerControllercooldownV

--- Assets/JDGSP/Scripts/script-ss/PlayerController.cs
+++ Assets/JDGSP/Scripts/script-ss/PlayerController.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpHeight = 2f;
+    public float sprintSpeed = 10f;
+    public float sprintDuration = 7f;
+    public float sprintCooldown = 5f;
+    public float jumpHeight = 4f;
+    public float jumpCooldown = 1.5f;
     public float gravity = -9.81f;
 
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
-
+    private bool isSprinting = false;
+    private float sprintTimer = 0f;
+    private float sprintCooldownTimer = 0f;
+    private float jumpTimer = 0f;
 
     void Start()
     {
@@ -34,22 +41,46 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
-        controller.Move(move * speed * Time.deltaTime);
+
+        if (isSprinting && sprintTimer > 0f)
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+            sprintTimer -= Time.deltaTime;
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (sprintCooldownTimer > 0f)
+            {
+                sprintCooldownTimer -= Time.deltaTime;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    isSprinting = true;
+                    sprintTimer = sprintDuration;
+                    sprintCooldownTimer = sprintCooldown;
+                }
+            }
+        }
 
         // Player jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded && jumpTimer <= 0f)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -1f * gravity);
+            jumpTimer = jumpCooldown;
         }
 
         // Apply gravity
         playerVelocity.y += gravity * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
-        
+        if (jumpTimer > 0f)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
     }
 }
+
